f31cf7d1ad7b0196079a11ac0486c90b7b76746a
shareifjones <shareifjones@gmail.com>
2024-08-20 00:34:48 -0400

    working through 4.4

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,17 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+const MESSAGES_TO_IGNORE = [
+    "When testing, code that causes React state updates should be wrapped into act(...):",
+    "Error:",
+    "The above error occurred",
+    "Warning: `ReactDOMTestUtils.act` is deprecated in favor of `React.act`. Import `act` from `react` instead of `react-dom/test-utils`. See https://react.dev/warnings/react-dom-test-utils for more info."
+];
+
+const originalError = console.error.bind(console.error);
+
+console.error = (...args) => {
+    const ignoreMessage = MESSAGES_TO_IGNORE.find(message => args.toString().includes(message));
+    if (!ignoreMessage) originalError(...args);
+}
