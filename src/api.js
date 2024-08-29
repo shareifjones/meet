7567aeeeb4985730b51ac7fc2d5a7c8a1345d656
shareifjones <shareifjones@gmail.com>
2024-08-29 15:35:28 -0400

    4.9

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 import mockData from "./mock-data";
+import NProgress from "nprogress";
 
 /**
  *
@@ -51,6 +52,7 @@ const checkToken = async (accessToken) => {
  * This function will fetch the list of all events
  */
 export const getEvents = async () => {
+
 
     if (window.location.href.startsWith("http://localhost")) {
         return mockData;
